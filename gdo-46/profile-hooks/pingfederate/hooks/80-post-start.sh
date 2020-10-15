07ad02cc835fa2aa5c925987f5e21eadd5418c4e
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-10-15 09:32:35 -0500

    compare to curl wait for

--- gdo-46/profile-hooks/pingfederate/hooks/80-post-start.sh
+++ gdo-46/profile-hooks/pingfederate/hooks/80-post-start.sh
@@ -11,8 +11,21 @@
 if test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE" -o "${OPERATIONAL_MODE}" = "STANDALONE" 
 then
     echo "INFO: waiting for healthy admin before post-start.."
-    wait-for "localhost:${PF_ADMIN_PORT}" -t 200
-    test ${?} -ne 0 && kill 1
+    _adminReady=false
+    until "${_adminReady}" ; do
+      _success=$(
+        curl -k
+          --write-out '%{http_code}'
+          https://localhost:${PF_ADMIN_PORT}/pingfederate/app
+        )
+      if test "${_success}" = "200" ; then
+        _adminReady=true
+      else
+        sleep 1
+      fi
+    done
+    # wait-for "localhost:${PF_ADMIN_PORT}" -t 200
+    # test ${?} -ne 0 && kill 1
     "${HOOKS_DIR}/81-after-start-process.sh"
     test ${?} -ne 0 && kill 1
 
