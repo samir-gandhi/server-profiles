a7ef6ece85f428668ca74c075580ad4a11cb53e9
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2021-10-27 13:50:32 -0600

    release name var

--- gdo-822/pingtoolkit/hooks/upgrade-pf.sh
+++ gdo-822/pingtoolkit/hooks/upgrade-pf.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
+env
+
 ## ENV VARS: 
 PING_IDENTITY_DEVOPS_USER
 PING_IDENTITY_DEVOPS_KEY
 NEW_PF_VERSION="${NEW_PF_VERSION:-10.3.1}"
+RELEASE_NAME=${RELEASE_NAME:=sg-822}
 
 ## GET new product bits and license
     ## download product to /tmp/pingfederate-<version>.zip
@@ -13,7 +16,7 @@ unzip -d /opt/new "/tmp/pingfederate-${NEW_PF_VERSION}.zip"
 sh /opt/staging/hooks/get-bits.sh -p pingfederate -v "${NEW_PF_VERSION}" -u "${PING_IDENTITY_DEVOPS_USER}" -k "${PING_IDENTITY_DEVOPS_KEY}" -l -c
 
 ## cp out-dir from admin
-pfPodName=$(kubectl get pod --selector=app.kubernetes.io/instance=gdo-822 --selector=app.kubernetes.io/name=pingfederate-admin -o=jsonpath='{.items[*].metadata.name}')
+pfPodName=$(kubectl get pod --selector=app.kubernetes.io/instance=${RELEASE_NAME} --selector=app.kubernetes.io/name=pingfederate-admin -o=jsonpath='{.items[*].metadata.name}')
 
 mkdir -p /opt/current /opt/current_bak
 kubectl cp ${pfPodName}:/opt/out /opt/current_bak
@@ -21,3 +24,5 @@ cp -r /opt/current_bak/instance /opt/current/pingfederate
 
 cd /opt/new/pingfederate-${NEW_PF_VERSION}/pingfederate/upgrade/bin
 sh upgrade.sh /opt/current -l /tmp/pingfederate.lic --release-notes-reviewed
+
+diff -r /opt/new/pingfederate-${NEW_PF_VERSION}/pingfederate/server/default/data /opt/current/pingfederate/server/default/data
