25b35c47a5060aeabbdba7547ebef6e88fd0b282
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2021-10-27 15:45:31 -0600

    better wait

--- gdo-822/pingtoolkit/hooks/upgrade-pf.sh
+++ gdo-822/pingtoolkit/hooks/upgrade-pf.sh
@@ -21,29 +21,30 @@ pfPodName=$(kubectl get pod --selector=app.kubernetes.io/instance=${RELEASE} --s
 ## patch pf-admin sts to turn off admin.  
 ##TODO: cleaner resource name, should be var. 
 kubectl set env sts/sg-822-pingfederate-admin STARTUP_COMMAND="tail" STARTUP_FOREGROUND_OPTS="-f /dev/null"
-_timeoutElapsed=1
-_timeout=1000
-## wait for health
-  while test $_timeoutElapsed -lt $_timeout ; do
-    sleep 6
-    if test $(kubectl get pods -l app.kubernetes.io/instance="${RELEASE}" -o go-template='{{range $index, $element := .items}}{{range .status.containerStatuses}}{{if not .ready}}{{$element.metadata.name}}{{"\n"}}{{end}}{{end}}{{end}}' | wc -l ) = 0 ; then
-      readyCount=$(( readyCount+1 ))
-      sleep 4
-    else 
-      crashingPods=$(kubectl get pods -l app.kubernetes.io/instance="${RELEASE}" -n "${K8S_NAMESPACE}" -o go-template='{{range $index, $element := .items}}{{range .status.containerStatuses}}{{if gt .restartCount 2 }}{{$element.metadata.name}}{{"\n"}}{{end}}{{end}}{{end}}')
-      numCrashing=$(echo "${crashingPods}" |wc -c)
-      if test $numCrashing -gt 5 ; then
-        echo "ERROR: Found pods crashing $crashingPods"
-        _timeoutElapsed=$(( _timeout+1 ))
-      fi
-    fi
-    if test ${readyCount} -ge 3 ; then
-      echo "INFO: Successfully Deployed."
-      break
-    fi
-    _timeoutElapsed=$((_timeoutElapsed+6))
-  done
+echo "waiting for running pod"
+until test "$(kubectl get po "${pfPodName}" -o=jsonpath='{@.status.phase}')" = "Running" ; do
+  sleep 2
+done
 
+#TODO: watch out for crashing pods.
+  #   sleep 6
+  #   if test $(kubectl get pods -l app.kubernetes.io/instance="${RELEASE}" -o go-template='{{range $index, $element := .items}}{{range .status.containerStatuses}}{{if not .ready}}{{$element.metadata.name}}{{"\n"}}{{end}}{{end}}{{end}}' | wc -l ) = 0 ; then
+  #     readyCount=$(( readyCount+1 ))
+  #     sleep 4
+  #   else 
+  #     crashingPods=$(kubectl get pods -l app.kubernetes.io/instance="${RELEASE}" -n "${K8S_NAMESPACE}" -o go-template='{{range $index, $element := .items}}{{range .status.containerStatuses}}{{if gt .restartCount 2 }}{{$element.metadata.name}}{{"\n"}}{{end}}{{end}}{{end}}')
+  #     numCrashing=$(echo "${crashingPods}" |wc -c)
+  #     if test $numCrashing -gt 5 ; then
+  #       echo "ERROR: Found pods crashing $crashingPods"
+  #       _timeoutElapsed=$(( _timeout+1 ))
+  #     fi
+  #   fi
+  #   if test ${readyCount} -ge 3 ; then
+  #     echo "INFO: Successfully Deployed."
+  #     break
+  #   fi
+  #   _timeoutElapsed=$((_timeoutElapsed+6))
+  # done
 
 mkdir -p /opt/current /opt/current_bak
 kubectl cp ${pfPodName}:/opt/out /opt/current_bak
@@ -60,3 +61,8 @@ diff -r /opt/new/pingfederate-${NEW_PF_VERSION}/pingfederate/server/default/data
 
 # kubectl exec -it ${pfPodName} -- rm -rf /opt/out/instance/server/default/default/data/*
 # kubectl cp "/opt/new/pingfederate-${NEW_PF_VERSION}/pingfederate/server/default/data" ${pfPodName}:/opt/out/instance/server/default/data
+
+##Restart admin
+##TODO: cleaner resource name, should be var. 
+kubectl set env sts/sg-822-pingfederate-admin STARTUP_COMMAND- STARTUP_FOREGROUND_OPTS-
+kubectl delete pod ${pfPodName}
