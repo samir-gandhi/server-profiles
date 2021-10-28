555649babb8885b348033aa7d7793f1abfa34834
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2021-10-28 14:21:57 -0600

    kubectl patch

--- gdo-822/pingtoolkit/hooks/upgrade-pf.sh
+++ gdo-822/pingtoolkit/hooks/upgrade-pf.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 
+set -e
+
 env
 
 ## ENV VARS: 
 # PING_IDENTITY_DEVOPS_USER
 # PING_IDENTITY_DEVOPS_KEY
-NEW_PF_VERSION="${NEW_PF_VERSION:-10.3.1}"
-RELEASE=${RELEASE:=sg-822}
+NEW_PF_VERSION="${NEW_PF_VERSION:-10.3.2}"
+RELEASE=${RELEASE:-sg-822}
+PF_ADMIN_PRIVATE_HOSTNAME=${PF_ADMIN_PRIVATE_HOSTNAME:-sg-822-pingfederate-admin}
+PF_ADMIN_PRIVATE_PORT_HTTPS=${PF_ADMIN_PRIVATE_PORT_HTTPS:-9999}
 
 ## GET new product bits and license
     ## download product to /tmp/pingfederate-<version>.zip
@@ -15,39 +19,24 @@ unzip -d /opt/new "/tmp/pingfederate-${NEW_PF_VERSION}.zip"
   ## download license to /tmp/pingfederate.lic
 sh /opt/staging/hooks/get-bits.sh -p pingfederate -v "${NEW_PF_VERSION}" -u "${PING_IDENTITY_DEVOPS_USER}" -k "${PING_IDENTITY_DEVOPS_KEY}" -l -c
 
-## cp out-dir from admin
+## get admin pod name
 pfPodName=$(kubectl get pod --selector=app.kubernetes.io/instance=${RELEASE} --selector=app.kubernetes.io/name=pingfederate-admin -o=jsonpath='{.items[*].metadata.name}')
 
 ## patch pf-admin sts to turn off admin.  
-##TODO: cleaner resource name, should be var. 
-kubectl set env sts/sg-822-pingfederate-admin STARTUP_COMMAND="tail" STARTUP_FOREGROUND_OPTS="-f /dev/null"
+kubectl patch sts sg-822-pingfederate-admin --patch "$( cat /opt/staging/hooks/pf-patch.yaml )"
+
 echo "waiting for running pod"
 
 ## TODO: make this better
 sleep 45
-until test "$(kubectl get po "${pfPodName}" -o=jsonpath='{@.status.phase}')" = "Running" ; do
+until test $(curl -ksS https://${PF_ADMIN_PRIVATE_HOSTNAME}:${PF_ADMIN_PRIVATE_PORT_HTTPS}/pingfederate/app --write-out '%{http_code}' --output /dev/null) -eq 200 ; do
   sleep 2
 done
 
-#TODO: watch out for crashing pods.
-  #   sleep 6
-  #   if test $(kubectl get pods -l app.kubernetes.io/instance="${RELEASE}" -o go-template='{{range $index, $element := .items}}{{range .status.containerStatuses}}{{if not .ready}}{{$element.metadata.name}}{{"\n"}}{{end}}{{end}}{{end}}' | wc -l ) = 0 ; then
-  #     readyCount=$(( readyCount+1 ))
-  #     sleep 4
-  #   else 
-  #     crashingPods=$(kubectl get pods -l app.kubernetes.io/instance="${RELEASE}" -n "${K8S_NAMESPACE}" -o go-template='{{range $index, $element := .items}}{{range .status.containerStatuses}}{{if gt .restartCount 2 }}{{$element.metadata.name}}{{"\n"}}{{end}}{{end}}{{end}}')
-  #     numCrashing=$(echo "${crashingPods}" |wc -c)
-  #     if test $numCrashing -gt 5 ; then
-  #       echo "ERROR: Found pods crashing $crashingPods"
-  #       _timeoutElapsed=$(( _timeout+1 ))
-  #     fi
-  #   fi
-  #   if test ${readyCount} -ge 3 ; then
-  #     echo "INFO: Successfully Deployed."
-  #     break
-  #   fi
-  #   _timeoutElapsed=$((_timeoutElapsed+6))
-  # done
+## Kill the PF process
+kubectl exec ${pfPodName} -- pkill /opt/java/bin/java
+## let pf shutdown
+sleep 15
 
 mkdir -p /opt/current /opt/current_bak
 kubectl cp ${pfPodName}:/opt/out /opt/current_bak
@@ -56,14 +45,14 @@ cp -r /opt/current_bak/instance /opt/current/pingfederate
 cd /opt/new/pingfederate-${NEW_PF_VERSION}/pingfederate/upgrade/bin
 sh upgrade.sh /opt/current -l /tmp/pingfederate.lic --release-notes-reviewed
 
-diff -r /opt/new/pingfederate-${NEW_PF_VERSION}/pingfederate/server/default/data /opt/current/pingfederate/server/default/data
+diff -r /opt/current/pingfederate/server/default/data /opt/current_bak/pingfederate/server/default/data
 
 # echo_red "REALLY BAD SECURITY PRACTICE, WHY ARE YOU DOING THIS!?"
 
 
 
-kubectl exec ${pfPodName} -- sh -c 'rm -rf /opt/out/instance/server/default/data/*'
-kubectl cp "/opt/new/pingfederate-${NEW_PF_VERSION}/pingfederate/server/default/data" ${pfPodName}:/opt/out/instance/server/default/data/.
+# kubectl exec ${pfPodName} -- sh -c 'rm -rf /opt/out/instance/server/default/data/*'
+# kubectl cp "/opt/new/pingfederate-${NEW_PF_VERSION}/pingfederate/server/default/data" ${pfPodName}:/opt/out/instance/server/default/data/.
 
 ## Ready for helm upgrade 10.3.2 now
 ##TODO: cleaner resource name, should be var. 
