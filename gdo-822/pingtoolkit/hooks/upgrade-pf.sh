17cef21c8329044e601376f88875c249cc5c4105
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2021-10-29 08:50:09 -0600

    diff staging files

--- gdo-822/pingtoolkit/hooks/upgrade-pf.sh
+++ gdo-822/pingtoolkit/hooks/upgrade-pf.sh
@@ -22,12 +22,12 @@ sh /opt/staging/hooks/get-bits.sh -p pingfederate -v "${NEW_PF_VERSION}" -u "${P
 ## get admin pod name
 pfPodName=$(kubectl get pod --selector=app.kubernetes.io/instance=${RELEASE} --selector=app.kubernetes.io/name=pingfederate-admin -o=jsonpath='{.items[*].metadata.name}')
 
-## patch pf-admin sts to turn off admin.  
+## patch pf-admin sts to start admin in background mode
 kubectl patch sts sg-822-pingfederate-admin --patch "$( cat /opt/staging/hooks/pf-patch.yaml )"
 
 echo "waiting for running pod"
 
-## TODO: make this better
+## TODO: make this more efficient
 sleep 45
 until test $(curl -ksS https://${PF_ADMIN_PRIVATE_HOSTNAME}:${PF_ADMIN_PRIVATE_PORT_HTTPS}/pingfederate/app --write-out '%{http_code}' --output /dev/null) -eq 200 ; do
   sleep 2
@@ -40,12 +40,24 @@ sleep 15
 
 mkdir -p /opt/current /opt/current_bak
 kubectl cp ${pfPodName}:/opt/out /opt/current_bak
+kubectl cp ${pfPodName}:/opt/staging /opt/staging_bak
 cp -r /opt/current_bak/instance /opt/current/pingfederate
 
 cd /opt/new/pingfederate-${NEW_PF_VERSION}/pingfederate/upgrade/bin
 sh upgrade.sh /opt/current -l /tmp/pingfederate.lic --release-notes-reviewed
 
 diff -r /opt/current/pingfederate/server/default/data /opt/current_bak/pingfederate/server/default/data
+
+## Profile Diff:
+set -x
+stgFiles=$(find /opt/staging_bak/instance/ -type f)
+for f in $stgFiles ; do 
+  echo "$f" |  cut -d"/" -f5- >> /tmp/stagingFileList
+done
+while read -r line; do 
+  diff "/opt/staging_bak/instance/${line}" "/opt/current/instance/${line}"
+done < /tmp/stagingFileList
+
 
 # echo_red "REALLY BAD SECURITY PRACTICE, WHY ARE YOU DOING THIS!?"
 
